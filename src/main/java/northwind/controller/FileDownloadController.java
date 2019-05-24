6ef2e76e5bc5d4104efcda14f2726aacc3c3538f
Sameer Sarmah <sameerparansarmah@gmail.com>
2019-05-24 13:06:14 +0530

    zipping the excel + added tests

--- src/main/java/northwind/controller/FileDownloadController.java
+++ src/main/java/northwind/controller/FileDownloadController.java
@@ -1,13 +1,20 @@
 package northwind.controller;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Type;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
@@ -51,16 +58,29 @@ public class FileDownloadController {
     
     @RequestMapping( value = "/downloadxls",method = RequestMethod.GET)
     public void downloadTextExcel( HttpServletRequest request,HttpServletResponse response) {
-    	String contentType="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+    	//String contentType="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+    	String contentType="application/zip";
     	String webInfPath = request.getServletContext().getRealPath("/");
     	String fileName=excelWriter.write(webInfPath,getProducts());
-    	downloadFile(request,response,fileName,contentType);
-    	String filePath =webInfPath+ File.separator+fileName;
-    	File file = new File(filePath);
-    	if(file.exists()) {
-    		boolean isDeleted = file.delete();
-    		System.out.println(isDeleted);
-    	}
+    	try {
+			String zipName = zip(webInfPath,Arrays.asList(fileName));
+	    	downloadFile(request,response,zipName,contentType);
+	    	String filePath =webInfPath+ File.separator+fileName;
+	    	File excelFile = new File(filePath);
+	    	if(excelFile.exists()) {
+	    		boolean isDeleted = excelFile.delete();
+	    		System.out.println("excel file deleted " + isDeleted);
+	    	}
+	    	
+	    	File zipfile = new File(zipName);
+	    	if(zipfile.exists()) {
+	    		boolean isDeleted = zipfile.delete();
+	    		System.out.println("zip file deleted " + isDeleted);
+	    	}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
     	
     }
     
@@ -117,5 +137,60 @@ public class FileDownloadController {
         }
     }
 
+	
+	private static String zip(String contextPath,List<String> filesToZip) throws IOException {
+		long timestamp =new Date().getTime();
+		String zipName="Products_"+timestamp+".zip";
+		String zipPath = contextPath+ File.separator+zipName;
+		FileOutputStream fos = new FileOutputStream(zipPath);
+		ZipOutputStream zipOut = new ZipOutputStream(fos);
+		for (String fileToZip : filesToZip) {
+			String filePath = contextPath+ File.separator + fileToZip;
+	        File zip = new File(filePath);
+	        FileInputStream fis = new FileInputStream(filePath);
+	        ZipEntry zipEntry = new ZipEntry(zip.getName());
+	        zipOut.putNextEntry(zipEntry);
+	        byte[] bytes = new byte[1024];
+	        int length;
+	        while((length = fis.read(bytes)) >= 0) {
+	            zipOut.write(bytes, 0, length);
+	        }
+	        fis.close();
+		}
+		zipOut.close();
+        fos.close();
+		return zipName;
+	}
+	
+	private static void unzip(String unzipDir,String zipName) throws IOException {
+        File destDir = new File(unzipDir);
+        byte[] buffer = new byte[1024];
+        ZipInputStream zis = new ZipInputStream(new FileInputStream(zipName));
+        ZipEntry zipEntry = zis.getNextEntry();
+        while (zipEntry != null) {
+            File newFile = unzipFile(destDir, zipEntry);
+            FileOutputStream fos = new FileOutputStream(newFile);
+            int len;
+            while ((len = zis.read(buffer)) > 0) {
+                fos.write(buffer, 0, len);
+            }
+            fos.close();
+            zipEntry = zis.getNextEntry();
+        }
+        zis.closeEntry();
+        zis.close();
+	}
+	
+    public static File unzipFile(File destinationDir, ZipEntry zipEntry) throws IOException {
+        File destFile = new File(destinationDir, zipEntry.getName());
+        String destDirPath = destinationDir.getCanonicalPath();
+        String destFilePath = destFile.getCanonicalPath();
+         
+        if (!destFilePath.startsWith(destDirPath + File.separator)) {
+            throw new IOException("Entry is outside of the target dir: " + zipEntry.getName());
+        }
+        System.out.println(destFile.getName()+" unzipped");
+        return destFile;
+    }
 
 }
--- src/test/java/northwind/controller/test/FileDownloadControllerTest.java
+++ src/test/java/northwind/controller/test/FileDownloadControllerTest.java
@@ -5,7 +5,10 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Date;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
@@ -28,9 +31,6 @@ import northwind.controller.FileDownloadController;
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebMvcTest({FileDownloadController.class})
 public class FileDownloadControllerTest {
-
-//    @Autowired
-//    private RestTemplate restTemplate;
     
     @Autowired
     private MockMvc mockMvc;
@@ -43,15 +43,25 @@ public class FileDownloadControllerTest {
     	try {
 			MvcResult response = mockMvc.perform(get("/downloadxls")).andReturn();
 			byte[] bytes = response.getResponse().getContentAsByteArray();
-			long timestamp =new Date().getTime();
-			String fileName="Products_"+timestamp+".xlsx";
-			File file = new File(fileName);
-			FileOutputStream out = new FileOutputStream(file);
+			String zipfileName="Products.zip";
+			File zipfile = new File(zipfileName);
+			FileOutputStream out = new FileOutputStream(zipfile);
 			out.write(bytes);
 			out.flush();
 			out.close();
-			FileInputStream inStream = new FileInputStream(file);
-			Workbook workbook = new XSSFWorkbook(file);
+			unzip(".",zipfileName);
+			File cwd = new File(".");
+			String[] files = cwd.list();
+			String excelfileName=null;
+			for(String file:files) {
+				if(file.contains(".xlsx")) {
+					excelfileName = file;
+				}
+			}
+			
+			FileInputStream inStream = new FileInputStream(excelfileName);
+			File excelFile = new File(excelfileName);
+			Workbook workbook = new XSSFWorkbook(excelFile);
 			Sheet sheet = workbook.getSheet("Products");
 			int totalRows = sheet.getLastRowNum() - sheet.getFirstRowNum();
 			for(int rowIndex = 1 ;rowIndex <= totalRows ;rowIndex++) {
@@ -61,10 +71,14 @@ public class FileDownloadControllerTest {
 					Cell cell = row.getCell(colIndex);
 					if(cell != null) {
 						if(cell.getCellType().equals(CellType.NUMERIC)) {
-							Assert.assertNotNull(cell.getNumericCellValue());
+							double cellValue = cell.getNumericCellValue();
+							System.out.println(cellValue);
+							Assert.assertNotNull(cellValue);
 						}
 						else if(cell.getCellType().equals(CellType.STRING)) {
-							Assert.assertNotNull(cell.getStringCellValue());
+							String cellValue = cell.getStringCellValue();
+							System.out.println(cellValue);
+							Assert.assertNotNull(cellValue);
 						}
 					}
 
@@ -73,8 +87,12 @@ public class FileDownloadControllerTest {
 			}
 			inStream.close();
 			workbook.close();
-	    	if(file.exists()) {
-	    		boolean isDeleted = file.delete();
+	    	if(zipfile.exists()) {
+	    		boolean isDeleted = zipfile.delete();
+	    		System.out.println(isDeleted);
+	    	}
+	    	if(excelFile.exists()) {
+	    		boolean isDeleted = excelFile.delete();
 	    		System.out.println(isDeleted);
 	    	}
 			
@@ -85,4 +103,34 @@ public class FileDownloadControllerTest {
 		}
     }
 	
+	private static void unzip(String unzipDir,String zipName) throws IOException {
+        File destDir = new File(unzipDir);
+        byte[] buffer = new byte[1024];
+        ZipInputStream zis = new ZipInputStream(new FileInputStream(zipName));
+        ZipEntry zipEntry = zis.getNextEntry();
+        while (zipEntry != null) {
+            File newFile = unzipFile(destDir, zipEntry);
+            FileOutputStream fos = new FileOutputStream(newFile);
+            int len;
+            while ((len = zis.read(buffer)) > 0) {
+                fos.write(buffer, 0, len);
+            }
+            fos.close();
+            zipEntry = zis.getNextEntry();
+        }
+        zis.closeEntry();
+        zis.close();
+	}
+	
+    public static File unzipFile(File destinationDir, ZipEntry zipEntry) throws IOException {
+        File destFile = new File(destinationDir, zipEntry.getName());
+        String destDirPath = destinationDir.getCanonicalPath();
+        String destFilePath = destFile.getCanonicalPath();
+         
+        if (!destFilePath.startsWith(destDirPath + File.separator)) {
+            throw new IOException("Entry is outside of the target dir: " + zipEntry.getName());
+        }
+        System.out.println(destFile.getName()+" unzipped");
+        return destFile;
+    }
 }
